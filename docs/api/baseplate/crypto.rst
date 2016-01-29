ccf01afc0eddabcc1bada7d6ac5ee9ba8bfc3c2c
Neil Williams <neil@reddit.com>
2016-01-28 17:05:26 -0800

    Add crypto utils including message signing helper

---
+++ docs/api/baseplate/crypto.rst
@@ -0,0 +1,27 @@
+baseplate.crypto
+================
+
+.. automodule:: baseplate.crypto
+
+Message Signing
+---------------
+
+.. autoclass:: MessageSigner
+   :members:
+
+Exceptions
+~~~~~~~~~~
+
+.. autoexception:: SignatureError
+
+.. autoexception:: UnreadableSignatureError
+
+.. autoexception:: IncorrectSignatureError
+
+.. autoexception:: ExpiredSignatureError
+
+
+Utilities
+---------
+
+.. autofunction:: constant_time_compare
---
+++ tests/unit/crypto_tests.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import datetime
+import unittest
+
+from baseplate import crypto
+
+from .. import mock
+
+
+class ConstantTimeCompareTests(unittest.TestCase):
+    def check_equal(self):
+        self.assertTrue(crypto.constant_time_compare("abcdefg", "abcdefg"))
+
+    def check_inequal(self):
+        self.assertFalse(crypto.constant_time_compare("abcdefg", "hijklmnop"))
+
+
+class SignatureTests(unittest.TestCase):
+    def setUp(self):
+        self.message = "Hello, this is a message."
+        self.secret = b"abcdefg"
+        self.signer = crypto.MessageSigner(self.secret)
+
+    def test_roundtrip(self):
+        signature = self.signer.make_signature(self.message, max_age=datetime.timedelta(seconds=30))
+        self.signer.validate_signature(self.message, signature)
+
+    @mock.patch("time.time")
+    def test_expired(self, time):
+        time.return_value = 0
+        signature = self.signer.make_signature(self.message, max_age=datetime.timedelta(seconds=30))
+
+        time.return_value = 90
+        with self.assertRaises(crypto.ExpiredSignatureError):
+            self.signer.validate_signature(self.message, signature)
+
+    def test_unreadable(self):
+        signature = self.signer.make_signature(self.message, max_age=datetime.timedelta(seconds=30))
+
+        with self.assertRaises(crypto.UnreadableSignatureError):
+            self.signer.validate_signature(self.message, signature[2:])
+
+    def test_signature_urlsafe(self):
+        signature = self.signer.make_signature(self.message, max_age=datetime.timedelta(seconds=30))
+        self.assertTrue(b"=" not in signature)
