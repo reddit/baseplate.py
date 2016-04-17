8b12ae0d185e9c916a58c9c85f96664799a89b4d
Neil Williams <neil@reddit.com>
2016-04-17 14:50:23 -0700

    Add general retry mechanism with configurable policy
    This also refactors several places internally which did their own retry
    logic to use the new retry system.

---
+++ baseplate/retry.py
@@ -0,0 +1,128 @@
+"""Policies for retrying an operation safely."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import time
+
+
+class RetryPolicy(object):
+    """A policy for retrying operations.
+
+    Policies are meant to be used as an iterable::
+
+        for time_remaining in RetryPolicy.new(attempts=3):
+            try:
+                some_operation.do(timeout=time_remaining)
+                break
+            except SomeError:
+                pass
+        else:
+            raise MaxRetriesError
+
+    """
+
+    def yield_attempts(self):
+        """Return an iterator which controls attempts.
+
+        On each iteration, the iterator will yield the number of seconds left
+        to retry, this should be used to set the timeout on the operation
+        being carried out. If there is no maximum time remaining,
+        :py:data:`None` is yielded instead.
+
+        The iterable will raise :py:exc:`StopIteration` once the operation
+        should not be retried any further.
+
+        """
+
+        raise NotImplementedError
+
+    def __iter__(self):
+        """Convenience alias for :py:meth:`yield_attempts`.
+
+        This allows policies to be directly iterated over.
+
+        """
+        return self.yield_attempts()
+
+    @staticmethod
+    def new(attempts=None, budget=None, backoff=None):
+        """Create a new retry policy with the given constraints.
+
+        :param int attempts: The maximum number of times the operation can be
+            attempted.
+        :param float budget: The maximum amount of time, in seconds, that the
+            local service will wait for the operation to succeed.
+        :param float backoff: The base amount of time, in seconds, for
+            exponential backoff between attempts. ``N`` in (``N *
+            2**attempts``).
+
+        """
+        policy = IndefiniteRetryPolicy()
+
+        if attempts is not None:
+            policy = MaximumAttemptsRetryPolicy(policy, attempts)
+
+        if budget is not None:
+            policy = TimeBudgetRetryPolicy(policy, budget)
+
+        if backoff is not None:
+            policy = ExponentialBackoffRetryPolicy(policy, backoff)
+
+        return policy
+
+
+class IndefiniteRetryPolicy(RetryPolicy):  # pragma: noqa
+    """Retry immediately forever."""
+    def yield_attempts(self):
+        while True:
+            yield None
+
+
+class MaximumAttemptsRetryPolicy(RetryPolicy):
+    """Constrain the total number of attempts."""
+    def __init__(self, policy, attempts):
+        self.subpolicy = policy
+        self.attempts = attempts
+
+    def yield_attempts(self):
+        for i, remaining in enumerate(self.subpolicy):
+            if i == self.attempts:
+                break
+            yield remaining
+
+
+class TimeBudgetRetryPolicy(RetryPolicy):
+    """Constrain attempts to an overall time budget."""
+    def __init__(self, policy, budget):
+        self.subpolicy = policy
+        self.budget = budget
+
+    def yield_attempts(self):
+        start_time = time.time()
+
+        for _ in self.subpolicy:
+            elapsed = time.time() - start_time
+            time_remaining = self.budget - elapsed
+            if time_remaining <= 0:
+                break
+            yield time_remaining
+
+
+class ExponentialBackoffRetryPolicy(RetryPolicy):
+    """Sleep exponentially longer between attempts."""
+    def __init__(self, policy, base):
+        self.subpolicy = policy
+        self.base = base
+
+    def yield_attempts(self):
+        for attempt, time_remaining in enumerate(self.subpolicy):
+            if attempt > 0:
+                delay = self.base * 2**(attempt-1)
+                if time_remaining:
+                    delay = min(delay, time_remaining)
+                    time_remaining -= delay
+                time.sleep(delay)
+
+            yield time_remaining
---
+++ tests/unit/retry_tests.py
@@ -0,0 +1,116 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import itertools
+import unittest
+
+from baseplate.retry import (
+    ExponentialBackoffRetryPolicy,
+    IndefiniteRetryPolicy,
+    MaximumAttemptsRetryPolicy,
+    RetryPolicy,
+    TimeBudgetRetryPolicy,
+)
+
+from .. import mock
+
+
+class RetryPolicyTests(unittest.TestCase):
+    def test_maximum_attempts(self):
+        base_policy = mock.MagicMock()
+        base_policy.__iter__.return_value = itertools.repeat(1.2)
+        policy = MaximumAttemptsRetryPolicy(base_policy, attempts=3)
+
+        retries = iter(policy)
+        self.assertEqual(next(retries), 1.2)
+        self.assertEqual(next(retries), 1.2)
+        self.assertEqual(next(retries), 1.2)
+        with self.assertRaises(StopIteration):
+            next(retries)
+
+    @mock.patch("time.time", autospec=True)
+    def test_time_budget(self, time):
+        policy = TimeBudgetRetryPolicy(IndefiniteRetryPolicy(), budget=5)
+
+        time.return_value = 0
+        retries = iter(policy)
+        self.assertEqual(next(retries), 5)
+
+        time.return_value = 3
+        for _ in range(100):
+            self.assertEqual(next(retries), 2)
+
+        time.return_value = 7
+        with self.assertRaises(StopIteration):
+            next(retries)
+
+    @mock.patch("time.sleep", autospec=True)
+    def test_exponential_backoff(self, sleep):
+        base_policy = mock.MagicMock()
+        base_policy.__iter__.return_value = itertools.repeat(.9)
+        policy = ExponentialBackoffRetryPolicy(base_policy, base=.1)
+
+        retries = iter(policy)
+        next(retries)
+        self.assertEqual(sleep.call_count, 0)
+
+        next(retries)
+        sleep.assert_called_with(.1)
+
+        next(retries)
+        sleep.assert_called_with(.2)
+
+        next(retries)
+        sleep.assert_called_with(.4)
+
+        next(retries)
+        sleep.assert_called_with(.8)
+
+        # the base policy's lower time remaining takes over here
+        next(retries)
+        sleep.assert_called_with(.9)
+
+
+class ComplexPolicyTests(unittest.TestCase):
+    @mock.patch("time.sleep", autospec=True)
+    def test_attempts_and_backoff(self, sleep):
+        policy = RetryPolicy.new(backoff=0.2, attempts=3)
+        retries = iter(policy)
+
+        next(retries)
+
+        next(retries)
+        sleep.assert_called_with(0.2)
+
+        next(retries)
+        sleep.assert_called_with(0.4)
+
+        with self.assertRaises(StopIteration):
+            next(retries)
+
+    @mock.patch("time.time", autospec=True)
+    @mock.patch("time.sleep", autospec=True)
+    def test_budget_overrides_backoff(self, sleep, time):
+        policy = RetryPolicy.new(backoff=0.1, budget=1)
+
+        time.return_value = 0
+        retries = iter(policy)
+        time_remaining = next(retries)
+        self.assertAlmostEqual(time_remaining, 1)
+        self.assertEqual(sleep.call_count, 0)
+
+        time.return_value = .5
+        time_remaining = next(retries)
+        self.assertAlmostEqual(time_remaining, .4)
+        sleep.assert_called_with(.1)
+
+        time.return_value = .9
+        time_remaining = next(retries)
+        self.assertAlmostEqual(time_remaining, 0)
+        self.assertAlmostEqual(sleep.call_args[0][0], 0.1, places=2)
+
+        time.return_value = 1
+        with self.assertRaises(StopIteration):
+            next(retries)
