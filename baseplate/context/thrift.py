29f44eeb71c5a2e4f1e14281a58ab65c7246b0f1
Neil Williams <neil@reddit.com>
2016-04-17 14:54:18 -0700

    Add retry sugar to thrift context object client
    This sugar allows for Thrift-specific (i.e. knowledge of exception
    types) retries to be done on service clients. The reason for using a
    context manager here is to hopefully reduce the chance that a copy of
    the proxy client with special retry policy is saved for later and
    accidentally is used for retries on non-idempotent operations.

--- baseplate/context/thrift.py
+++ baseplate/context/thrift.py
@@ -3,10 +3,14 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import contextlib
 import functools
 import inspect
 
+from thrift.transport.TTransport import TTransportException
+
 from . import ContextFactory
+from ..retry import RetryPolicy
 
 
 class ThriftContextFactory(ContextFactory):
@@ -21,6 +25,15 @@ class ThriftContextFactory(ContextFactory):
     :param baseplate.thrift_pool.ConnectionPool pool: The connection pool.
     :param client_cls: The class object of a Thrift-generated client class,
         e.g. ``YourService.Client``.
+
+    The proxy object has a ``retrying`` method which takes the same parameters
+    as :py:meth:`RetryPolicy.new <baseplate.retry.RetryPolicy.new>` and acts as
+    a context manager. The context manager returns another proxy object where
+    Thrift service method calls will be automatically retried with the
+    specified retry policy when transient errors occur::
+
+        with context.my_service.retrying(attempts=3) as svc:
+            svc.some_method()
 
     """
     def __init__(self, pool, client_cls):
@@ -52,25 +65,48 @@ def _enumerate_service_methods(client):
 class PooledClientProxy(object):
     """A proxy which acts like a thrift client but uses a connection pool."""
 
-    def __init__(self, client_cls, pool, root_span, namespace):
+    def __init__(self, client_cls, pool, root_span, namespace, retry_policy=None):
         self.client_cls = client_cls
         self.pool = pool
         self.root_span = root_span
         self.namespace = namespace
+        self.retry_policy = retry_policy or RetryPolicy.new(attempts=1)
 
         for name in _enumerate_service_methods(client_cls):
             setattr(self, name, functools.partial(
                 self._call_thrift_method, name))
 
+    @contextlib.contextmanager
+    def retrying(self, **policy):
+        yield PooledClientProxy(
+            self.client_cls,
+            self.pool,
+            self.root_span,
+            self.namespace,
+            retry_policy=RetryPolicy.new(**policy),
+        )
+
     def _call_thrift_method(self, name, *args, **kwargs):
         trace_name = "{}.{}".format(self.namespace, name)
+        last_error = None
 
-        with self.root_span.make_child(trace_name) as span:
-            with self.pool.connection() as prot:
-                prot.trans.set_header("Trace", str(span.trace_id))
-                prot.trans.set_header("Parent", str(span.parent_id))
-                prot.trans.set_header("Span", str(span.id))
+        for _ in self.retry_policy:
+            try:
+                with self.root_span.make_child(trace_name) as span:
+                    with self.pool.connection() as prot:
+                        prot.trans.set_header("Trace", str(span.trace_id))
+                        prot.trans.set_header("Parent", str(span.parent_id))
+                        prot.trans.set_header("Span", str(span.id))
 
-                client = self.client_cls(prot)
-                method = getattr(client, name)
-                return method(*args, **kwargs)
+                        client = self.client_cls(prot)
+                        method = getattr(client, name)
+                        return method(*args, **kwargs)
+            except TTransportException as exc:
+                last_error = exc
+                continue
+        else:
+            raise TTransportException(
+                type=TTransportException.TIMED_OUT,
+                message="retry policy exhausted while attempting operation, "
+                        "last error was: {}".format(last_error),
+            )
