7c3d1cf86fba5ea455841c6e258a0d39a96eba53
Neil Williams <neil@reddit.com>
2016-05-24 11:30:50 -0700

    Cut version 0.11.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ extras_require = {
 
 setup(
     name="baseplate",
-    version="0.10.0",
+    version="0.11.0",
     packages=find_packages(exclude=["tests"]),
 
     install_requires=install_requires,
