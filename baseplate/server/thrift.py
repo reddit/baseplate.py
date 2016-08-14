0ecb22e1af5f6d5fdb0f0531e10d528c5b314e49
Neil Williams <neil@reddit.com>
2016-08-13 21:01:02 -0700

    Thrift server: Allow non-THeaderProtocol clients
    FBThrift's THeaderProtocol implementation has support for basic
    TBinaryProtocol (framed and unframed) clients but leaves it disabled by
    default. By flagging it on, we can transition to alternative protocols
    more easily down the line.

--- baseplate/server/thrift.py
+++ baseplate/server/thrift.py
@@ -10,6 +10,7 @@ from gevent.server import StreamServer
 from thrift.protocol.THeaderProtocol import THeaderProtocolFactory
 from thrift.server.TServer import TRpcConnectionContext
 from thrift.transport.TSocket import TSocket
+from thrift.transport.THeaderTransport import THeaderTransport
 from thrift.transport.TTransport import (
     TTransportException, TBufferedTransportFactory)
 
@@ -19,7 +20,14 @@ class GeventServer(StreamServer):
     def __init__(self, processor, *args, **kwargs):
         self.processor = processor
         self.transport_factory = TBufferedTransportFactory()
-        self.protocol_factory = THeaderProtocolFactory()
+        self.protocol_factory = THeaderProtocolFactory(
+            # allow non-headerprotocol clients to talk with us
+            client_types=[
+                THeaderTransport.HEADERS_CLIENT_TYPE,
+                THeaderTransport.FRAMED_DEPRECATED,
+                THeaderTransport.UNFRAMED_DEPRECATED,
+            ],
+        )
         super(GeventServer, self).__init__(*args, **kwargs)
 
     def serve_forever(self, stop_timeout=None):
