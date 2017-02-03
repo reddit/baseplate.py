2a76595bd52bed7c1cab564d0a2bc9d881a1ecf0
Andrew Boyle <andrew.boyle@reddit.com>
2017-02-03 10:31:15 -0800

    Add `string_types` to baseplate._compat

--- baseplate/_compat.py
+++ baseplate/_compat.py
@@ -14,11 +14,13 @@ if sys.version_info.major == 3:  # pragma: nocover
     import queue
     from io import BytesIO
     range = range
+    string_types = str,
 else:  # pragma: nocover
     import ConfigParser as configparser
     import Queue as queue
     from cStringIO import StringIO as BytesIO
     range = xrange
+    string_types = basestring,
 
 
 __all__ = [
@@ -26,4 +28,5 @@ __all__ = [
     "queue",
     "BytesIO",
     "range",
+    "string_types",
 ]
