2b67217f67dfc9845d7a25527e45f6388e332478
Neil Williams <neil@reddit.com>
2017-05-09 08:04:15 -0700

    Cut version 0.18.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     author_email="neil@reddit.com",
     license="BSD",
     url="https://reddit.github.io/baseplate/",
-    version="0.17.1",
+    version="0.18.0",
 
     packages=find_packages(exclude=["tests", "tests.*"]),
 
