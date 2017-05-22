be5b433ae33323385e995685a2941e5408ffbd1d
Neil Williams <neil@reddit.com>
2017-05-22 16:18:04 -0700

    Cut version 0.19.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     author_email="neil@reddit.com",
     license="BSD",
     url="https://baseplate.readthedocs.io/en/stable/",
-    version="0.18.0",
+    version="0.19.0",
 
     packages=find_packages(exclude=["tests", "tests.*"]),
 
