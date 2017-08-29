8bafaa3a10370ee1fc79efd658821ab2395ac805
Jeff <jdost@users.noreply.github.com>
2017-08-29 10:32:32 -0700

    Add in alternative MessageQueue backed by redis
    This offers an alternative `MessageQueue` handler that takes a Redis
    pool and uses it for backing of the queue storage as opposed to the
    POSIX system used normally.
    
    Pros:
    * Increased limits for queue size
    * Some mechanism to back the queue up to disk (configuration dependent)
    * No maximum limit for messages and message size
    * No restriction on queue name (even supports empty strings as a key)
    
    Cons:
    * Blocking timeout behavior for reads is more restrictive
      (it uses the [`BLPOP`](https://redis.io/commands/blpop) which only allows
      integers for the seconds based timeout)
    * More overhead before a timeout is issued compared to the POSIX variant
    
    Notes:
    * When the timeout is zero, the redis backend will treat it as infinite,
      whereas the POSIX variant would treat it as an immediate return, so
      it now performs an immediate (i.e. non blocking `LPOP`).

--- baseplate/context/redis.py
+++ baseplate/context/redis.py
@@ -5,9 +5,10 @@ from __future__ import unicode_literals
 
 import redis
 import redis.client
+from math import ceil
 
 from . import ContextFactory
-from .. import config
+from .. import config, message_queue
 
 
 def pool_from_config(app_config, prefix="redis.", **kwargs):
@@ -148,3 +149,71 @@ class MonitoredRedisPipeline(redis.client.StrictPipeline):
     def execute(self, **kwargs):
         with self.server_span.make_child(self.trace_name):
             return super(MonitoredRedisPipeline, self).execute(**kwargs)
+
+
+class MessageQueue(object):
+    """A Redis-backed variant of :py:class:`~baseplate.message_queue.MessageQueue`.
+
+    :param name: can be any string.
+
+    :param client: should be a :py:class:`redis.ConnectionPool` or
+           :py:class:`redis.BlockingConnectionPool` from which a client
+           connection can be created from (preferably generated from the
+           :py:func:`pool_from_config` helper).
+
+    """
+    def __init__(self, name, client):
+        self.queue = name
+        if isinstance(client, redis.BlockingConnectionPool) or \
+                isinstance(client, redis.ConnectionPool):
+            self.client = redis.Redis(connection_pool=client)
+        else:
+            self.client = client
+
+    def get(self, timeout=None):
+        """Read a message from the queue.
+
+        :param int timeout: If the queue is empty, the call will block up to
+            ``timeout`` seconds or forever if ``None``, if a float is given,
+            it will be rounded up to be an integer
+        :raises: :py:exc:`TimedOutError` The queue was empty for the allowed
+            duration of the call.
+
+        """
+        if isinstance(timeout, float):
+            timeout = int(ceil(timeout))
+
+        if timeout == 0:
+            message = self.client.lpop(self.queue)
+        else:
+            message = self.client.blpop(self.queue, timeout=timeout)
+
+            if message:
+                message = message[1]
+
+        if not message:
+            raise message_queue.TimedOutError
+
+        return message
+
+    def put(self, message, timeout=None):
+        """Add a message to the queue.
+
+        :param message: will be typecast to a string upon storage and will come
+               out of the queue as a string regardless of what type they are
+               when passed into this method.
+        """
+        return self.client.rpush(self.queue, message)
+
+    def unlink(self):
+        """Not implemented for Redis variant
+        """
+        pass
+
+    def close(self):
+        """Close queue when finished
+
+        Will delete the queue from the Redis server (Note, can still enqueue
+        and dequeue as the actions will recreate the queue)
+        """
+        self.client.delete(self.queue)
