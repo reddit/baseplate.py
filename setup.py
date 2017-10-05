0fb7c227cbf604d00bce7953b9dd8ed5108fa3d5
Andrew Boyle <andrew.boyle@reddit.com>
2017-10-05 11:00:32 -0700

    Cut version 0.23.1

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     author_email="neil@reddit.com",
     license="BSD",
     url="https://baseplate.readthedocs.io/en/stable/",
-    version="0.23.0",
+    version="0.23.1",
 
     packages=find_packages(exclude=["tests", "tests.*"]),
 
