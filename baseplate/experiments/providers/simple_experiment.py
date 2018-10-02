bf3616fb5d5da62d573b10160daf81739c6515ed
Mike Doherty <mdoherty@reddit.com>
2018-10-02 13:17:02 -0700

    add override support to experiments

--- baseplate/experiments/providers/simple_experiment.py
+++ baseplate/experiments/providers/simple_experiment.py
@@ -27,6 +27,55 @@ variant_type_map = {
 }
 
 
+def _generate_overrides(override_config):
+    """Generate a dictionary of overrides.
+
+    The format of the override config is expected to be a list of dicts,
+    where each dict contains a single key (the treatment to assign) and value
+    (the targeting tree for that treatment).
+
+    This combination is used to ensure that the ordering of the overrides is
+    maintained, allowing consistent overriding for any overlapping area
+    across targeting trees. Each incoming variant request will check each
+    targeting override in sequence until one is matched or none match.
+
+    If the override configuration does not contain a wrapping list (ie, it
+    is just a dictionary), then overrides will work, but override ordering
+    is not guaranteed. This means that if there is overlap between overrides
+    (ie, a particular user is eligible for two overrides), then the results
+    may not be consistent.
+
+    In the event of an invalid override:
+    1) if a dict of variant : target is not provided, that list item will be
+        ignored. All other overrides will still remain active.
+    2) if the targeting object is invalid, the override will remain active,
+        but will return false for all targeting
+
+    Multiple targets can be provided for a single variant.
+    """
+
+    override_list = []
+    if override_config and isinstance(override_config, dict):
+        override_config = [override_config]
+
+    if override_config and isinstance(override_config, list):
+        for override_entry in override_config:
+            if not isinstance(override_entry, dict):
+                logger.error("Invalid override configuration. Not applying override."
+                    "Expected dictionary: {}".format(override_entry))
+                continue
+
+            if len(override_entry) > 1:
+                logger.warning("Multiple overrides in a single override entry. "
+                    "Execution order of overrides may not be consistent.")
+
+            for treatment, targeting_tree_cfg in override_entry.items():
+                targeting_tree = _generate_targeting(targeting_tree_cfg)
+                override_list.append({treatment: targeting_tree})
+
+    return override_list
+
+
 def _generate_targeting(targeting_config):
     """Generate the targeting tree for this experiment.
 
@@ -53,7 +102,7 @@ class SimpleExperiment(Experiment):
 
     def __init__(self, id, name, owner, start_ts, stop_ts, config,
                  experiment_version, shuffle_version, variant_set,
-                 bucket_seed, bucket_val, targeting, overrides, 
+                 bucket_seed, bucket_val, targeting, overrides,
                  enabled=True, log_bucketing=True, num_buckets=1000):
         """
         :param int id: The experiment id. This should be unique.
@@ -142,12 +191,8 @@ class SimpleExperiment(Experiment):
         targeting_config = config.get("targeting")
         targeting = _generate_targeting(targeting_config)
 
-        overrides = None
-        override_inputs = config.get("overrides")
-        if override_inputs and isinstance(override_inputs, dict):
-            overrides = {}
-            for variant, target in override_inputs:
-                overrides[variant] = _generate_targeting(target)
+        override_config = config.get("overrides")
+        overrides = _generate_overrides(override_config)
 
         return cls(
             id=id,
@@ -186,15 +231,19 @@ class SimpleExperiment(Experiment):
         return self._log_bucketing
 
     def is_targeted(self, **kwargs):
-        """Determine whether the provided kwargs match targeting parameters
+        """ Determine whether the provided kwargs match targeting parameters
         for this experiment.
         """
         return self._targeting.evaluate(**kwargs)
 
-    def is_overridden(self, **kwargs):
-        for variant, targeting in self._overrides:
-            if targeting.evaluate(**kwargs):
-                return variant
+    def get_override(self, **kwargs):
+        """ Determine whether the provided kwargs match targeting parameters
+        for forcing a particular variant.
+        """
+        for override_node in self._overrides:
+            for variant, targeting in override_node.items():
+                if targeting.evaluate(**kwargs):
+                    return variant
 
         return None
 
@@ -222,7 +271,7 @@ class SimpleExperiment(Experiment):
             )
             return None
 
-        override = is_overridden(**kwargs):
+        override = self.get_override(**kwargs)
         if override:
             return override
 
