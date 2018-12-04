897bb777146c737ef80d85aa8ba8d309b5004af3
Neil Williams <neil@reddit.com>
2018-12-04 15:56:29 -0800

    Cut version 0.28.4

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     author_email="neil@reddit.com",
     license="BSD",
     url="https://baseplate.readthedocs.io/en/stable/",
-    version="0.28.3",
+    version="0.28.4",
 
     packages=find_packages(exclude=["tests", "tests.*"]),
 
