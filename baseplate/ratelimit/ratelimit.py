98becf236e117fe99fff545736bc187dbdd24d57
Braden Groom <braden.groom@reddit.com>
2019-03-26 15:54:01 -0700

    fixup! Add ratelimiting

--- baseplate/ratelimit/ratelimit.py
+++ baseplate/ratelimit/ratelimit.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .backends.base import RateLimitBackend
+from .backends import RateLimitBackend
 from ..context import ContextFactory
 
 
