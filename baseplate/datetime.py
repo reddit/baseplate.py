ce6759adb7e5ddca92f40f19b67986f51e604d42
Mimi Chen <mimi.chen@reddit.com>
2019-06-05 11:07:37 -0700

    Reorder functions

--- baseplate/datetime.py
+++ baseplate/datetime.py
@@ -4,16 +4,16 @@ from datetime import datetime
 from datetime import timezone
 
 
-def datetime_to_epoch_seconds(dt: datetime) -> int:
-    """Convert datetime object to epoch seconds."""
-    return int(datetime_to_epoch_milliseconds(dt) / 1000)
-
-
 def datetime_to_epoch_milliseconds(dt: datetime) -> int:
     """Convert datetime object to epoch milliseconds."""
     if not dt.tzinfo:
         dt = dt.replace(tzinfo=timezone.utc)
     return int(dt.timestamp() * 1000)
+
+
+def datetime_to_epoch_seconds(dt: datetime) -> int:
+    """Convert datetime object to epoch seconds."""
+    return int(datetime_to_epoch_milliseconds(dt) / 1000)
 
 
 def epoch_seconds_to_datetime(sec: int) -> datetime:
