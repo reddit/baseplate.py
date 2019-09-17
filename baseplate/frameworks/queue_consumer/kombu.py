f9f616d6bed0ea6660a4d91916972c36605a727d
Andrew Boyle <andrew.boyle@reddit.com>
2019-09-17 11:26:06 -0700

    Add QueueConsumerServer
    This PR adds a new way to run queue consumers as a "server". This means that you will run your queue consumers using baseplate-serve rather than baseplate-script. The server allows you to process multiple messages concurrently (useful when you don't care about ordering and are IO bound rather than CPU bound) and also will attempt to shut down your consumers gracefully.
    
    Note that this approach largely sidesteps and deprecates the existing queue consumer flow rather than repurposing it.

---
+++ baseplate/frameworks/queue_consumer/kombu.py
@@ -0,0 +1,217 @@
+import logging
+import queue
+import socket
+
+from typing import Any
+from typing import Callable
+from typing import Optional
+from typing import Sequence
+from typing import TYPE_CHECKING
+
+import kombu
+
+from gevent.server import StreamServer
+from kombu.mixins import ConsumerMixin
+from kombu.transport.virtual import Channel
+
+from baseplate import Baseplate
+from baseplate import RequestContext
+from baseplate.clients.kombu import KombuSerializer
+from baseplate.server.queue_consumer import HealthcheckCallback
+from baseplate.server.queue_consumer import make_simple_healthchecker
+from baseplate.server.queue_consumer import MessageHandler
+from baseplate.server.queue_consumer import PumpWorker
+from baseplate.server.queue_consumer import QueueConsumerFactory
+
+
+if TYPE_CHECKING:
+    WorkQueue = queue.Queue[kombu.Message]  # pylint: disable=unsubscriptable-object
+else:
+    WorkQueue = queue.Queue
+
+logger = logging.getLogger(__name__)
+
+
+Handler = Callable[[RequestContext, Any, kombu.Message], None]
+
+
+class FatalMessageHandlerError(Exception):
+    """An error that signals that the queue process should exit.
+
+    Raising an Exception that is a subclass of FatalMessageHandlerError will
+    cause the KombuMessageHandler to re-raise the exception rather than swallowing
+    it which will cause the handler thread/process to stop.  This, in turn, will
+    gracefully shut down the QueueConsumerServer currently running.
+
+    Exceptions of this nature should be reserved for errors that are due to
+    problems with the environment rather than the message itself.  For example,
+    a node that cannot get its AWS credentials.
+    """
+
+
+class KombuConsumerWorker(ConsumerMixin, PumpWorker):
+    """Consumes messages from the given queues and pumps them into the internal work_queue.
+
+    This class does not directly implement the abstract `run` command from
+    PumpWorker because the ConsumerMixin class already defines it.
+    """
+
+    def __init__(
+        self,
+        connection: kombu.Connection,
+        queues: Sequence[kombu.Queue],
+        work_queue: WorkQueue,
+        serializer: Optional[KombuSerializer] = None,
+    ):
+        self.connection = connection
+        self.queues = queues
+        self.work_queue = work_queue
+        self.serializer = serializer
+
+    def get_consumers(self, Consumer: kombu.Consumer, channel: Channel) -> Sequence[kombu.Consumer]:
+        args = dict(queues=self.queues, on_message=self.work_queue.put)
+        if self.serializer:
+            args["accept"] = [self.serializer.name]
+        return [Consumer(**args)]
+
+    def stop(self) -> None:
+        logger.debug("Closing KombuConsumerWorker.")
+        # `should_stop` is an attribute of `ConsumerMixin`
+        self.should_stop = True
+
+
+class KombuMessageHandler(MessageHandler):
+    def __init__(self, baseplate: Baseplate, name: str, handler_fn: Handler):
+        self.baseplate = baseplate
+        self.name = name
+        self.handler_fn = handler_fn
+
+    def handle(self, message: kombu.Message) -> None:
+        context = self.baseplate.make_context_object()
+        try:
+            # We place the call to ``baseplate.make_server_span`` inside the
+            # try/except block because we still want Baseplate to see and
+            # handle the error (publish it to error reporting)
+            with self.baseplate.make_server_span(context, self.name) as span:
+                delivery_info = message.delivery_info
+                span.set_tag("kind", "consumer")
+                span.set_tag("amqp.routing_key", delivery_info.get("routing_key", ""))
+                span.set_tag("amqp.consumer_tag", delivery_info.get("consumer_tag", ""))
+                span.set_tag("amqp.delivery_tag", delivery_info.get("delivery_tag", ""))
+                span.set_tag("amqp.exchange", delivery_info.get("exchange", ""))
+                self.handler_fn(context, message.decode(), message)
+        except Exception as exc:
+            logger.exception(
+                "Unhandled error while trying to process a message.  The message "
+                "has been returned to the queue broker."
+            )
+            message.requeue()
+            if isinstance(exc, FatalMessageHandlerError):
+                logger.info("Recieved a fatal error, terminating the server.")
+                raise
+        else:
+            message.ack()
+
+
+class KombuQueueConsumerFactory(QueueConsumerFactory):
+    """Factory for running a :py:class:`~baseplate.server.queue_consumer.QueueConsumerServer` using Kombu.
+
+    For simple cases where you just need a basic queue with all the default
+    parameters for your message broker, you can use `KombuQueueConsumerFactory.new`.
+
+    If you need more control, you can create the :py:class:`~kombu.Queue` s yourself and
+    use the constructor directly.
+    """
+
+    def __init__(
+        self,
+        baseplate: Baseplate,
+        name: str,
+        connection: kombu.Connection,
+        queues: Sequence[kombu.Queue],
+        handler_fn: Handler,
+        health_check_fn: Optional[HealthcheckCallback] = None,
+        serializer: Optional[KombuSerializer] = None,
+    ):
+        """`KombuQueueConsumerFactory` constructor.
+
+        :param baseplate: The Baseplate set up for your consumer.
+        :param exchange: The `kombu.Exchange` that you will bind your :py:class:`~kombu.Queue` s
+            to.
+        :param queues: List of  :py:class:`~kombu.Queue` s to consume from.
+        :param queue_name: Name for your queue.
+        :param routing_keys: List of routing keys that you will create :py:class:`~kombu.Queue` s
+            to consume from.
+        :param handler_fn: A `baseplate.frameworks.queue_consumer.komub.Handler`
+            function that will process an individual message from a queue.
+        :param health_check_fn: A `baseplate.server.queue_consumer.HealthcheckCallback`
+            function that can be used to customize your health check.
+        :param serializer: A `baseplate.clients.kombu.KombuSerializer` that should
+            be used to decode the messages you are consuming.
+        """
+        self.baseplate = baseplate
+        self.connection = connection
+        self.queues = queues
+        self.name = name
+        self.handler_fn = handler_fn
+        self.health_check_fn = health_check_fn
+        self.serializer = serializer
+
+    @classmethod
+    def new(
+        cls,
+        baseplate: Baseplate,
+        exchange: kombu.Exchange,
+        connection: kombu.Connection,
+        queue_name: str,
+        routing_keys: Sequence[str],
+        handler_fn: Handler,
+        health_check_fn: Optional[HealthcheckCallback] = None,
+        serializer: Optional[KombuSerializer] = None,
+    ) -> "KombuQueueConsumerFactory":
+        """Return a new `KombuQueueConsumerFactory`.
+
+        This method will create the :py:class:`~kombu.Queue` s for you and is
+        appropriate to use in simple cases where you just need a basic queue with
+        all the default parameters for your message broker.
+
+        :param baseplate: The Baseplate set up for your consumer.
+        :param exchange: The `kombu.Exchange` that you will bind your
+            :py:class:`~kombu.Queue` s to.
+        :param exchange: The `kombu.Connection` to your message broker.
+        :param queue_name: Name for your queue.
+        :param routing_keys: List of routing keys that you will create
+            :py:class:`~kombu.Queue` s to consume from.
+        :param handler_fn: A `baseplate.frameworks.queue_consumer.komub.Handler`
+            function that will process an individual message from a queue.
+        :param health_check_fn: A `baseplate.server.queue_consumer.HealthcheckCallback`
+            function that can be used to customize your health check.
+        :param serializer: A `baseplate.clients.kombu.KombuSerializer` that should
+            be used to decode the messages you are consuming.
+        """
+        queues = []
+        for routing_key in routing_keys:
+            queues.append(kombu.Queue(name=queue_name, exchange=exchange, routing_key=routing_key))
+        return cls(
+            baseplate=baseplate,
+            name=queue_name,
+            connection=connection,
+            queues=queues,
+            handler_fn=handler_fn,
+            health_check_fn=health_check_fn,
+            serializer=serializer,
+        )
+
+    def build_pump_worker(self, work_queue: WorkQueue) -> KombuConsumerWorker:
+        return KombuConsumerWorker(
+            connection=self.connection,
+            queues=self.queues,
+            work_queue=work_queue,
+            serializer=self.serializer,
+        )
+
+    def build_message_handler(self) -> KombuMessageHandler:
+        return KombuMessageHandler(self.baseplate, self.name, self.handler_fn)
+
+    def build_health_checker(self, listener: socket.socket) -> StreamServer:
+        return make_simple_healthchecker(listener, callback=self.health_check_fn)
---
+++ tests/unit/frameworks/queue_consumer/kombu_tests.py
@@ -0,0 +1,197 @@
+import socket
+
+from queue import Queue
+from unittest import mock
+
+import kombu
+import pytest
+
+from gevent.server import StreamServer
+
+from baseplate import Baseplate
+from baseplate import RequestContext
+from baseplate import ServerSpan
+from baseplate.frameworks.queue_consumer.kombu import FatalMessageHandlerError
+from baseplate.frameworks.queue_consumer.kombu import KombuConsumerWorker
+from baseplate.frameworks.queue_consumer.kombu import KombuMessageHandler
+from baseplate.frameworks.queue_consumer.kombu import KombuQueueConsumerFactory
+
+from .... import does_not_raise
+
+
+@pytest.fixture
+def context():
+    return mock.Mock(spec=RequestContext)
+
+
+@pytest.fixture
+def span():
+    return mock.Mock(spec=ServerSpan)
+
+
+@pytest.fixture
+def baseplate(context, span):
+    bp = mock.MagicMock(spec=Baseplate)
+    bp._metrics_client = None
+    bp.make_server_span().__enter__.return_value = span
+    bp.make_context_object.return_value = context
+    # Reset the mock calls since setting up the span actually triggers a "call"
+    # to bp.make_server_span
+    bp.reset_mock()
+    return bp
+
+
+@pytest.fixture
+def name():
+    return "test-handler"
+
+
+class TestKombuMessageHandler:
+    @pytest.fixture
+    def message(self):
+        msg = mock.Mock(spec=kombu.Message)
+        msg.delivery_info = {
+            "routing_key": "routing-key",
+            "consumer_tag": "consumer-tag",
+            "delivery_tag": "delivery-tag",
+            "exchange": "exchange",
+        }
+        msg.decode.return_value = {"foo": "bar"}
+        return msg
+
+    def test_handle(self, context, span, baseplate, name, message):
+        handler_fn = mock.Mock()
+        handler = KombuMessageHandler(baseplate, name, handler_fn)
+        handler.handle(message)
+        baseplate.make_context_object.assert_called_once()
+        baseplate.make_server_span.assert_called_once_with(context, name)
+        baseplate.make_server_span().__enter__.assert_called_once()
+        span.set_tag.assert_has_calls(
+            [
+                mock.call("kind", "consumer"),
+                mock.call("amqp.routing_key", "routing-key"),
+                mock.call("amqp.consumer_tag", "consumer-tag"),
+                mock.call("amqp.delivery_tag", "delivery-tag"),
+                mock.call("amqp.exchange", "exchange"),
+            ],
+            any_order=True,
+        )
+        handler_fn.assert_called_once_with(context, message.decode(), message)
+        message.ack.assert_called_once()
+
+    @pytest.mark.parametrize(
+        "err,expectation",
+        [
+            (ValueError(), does_not_raise()),
+            (FatalMessageHandlerError(), pytest.raises(FatalMessageHandlerError)),
+        ],
+    )
+    def test_errors(self, err, expectation, baseplate, name, message):
+        def handler_fn(ctx, body, msg):
+            raise err
+
+        handler = KombuMessageHandler(baseplate, name, handler_fn)
+        with expectation:
+            handler.handle(message)
+        message.requeue.assert_called_once()
+
+
+@pytest.fixture
+def connection():
+    return mock.Mock(spec=kombu.Connection)
+
+
+@pytest.fixture
+def exchange():
+    return mock.Mock(spec=kombu.Exchange)
+
+
+@pytest.fixture
+def routing_keys():
+    return ["rk-1", "rk-2"]
+
+
+class TestQueueConsumerFactory:
+    @pytest.fixture
+    def make_queue_consumer_factory(self, baseplate, exchange, connection, name, routing_keys):
+        def _make_queue_consumer_factory(health_check_fn=None):
+            return KombuQueueConsumerFactory.new(
+                baseplate=baseplate,
+                exchange=exchange,
+                connection=connection,
+                queue_name=name,
+                routing_keys=routing_keys,
+                handler_fn=lambda ctx, body, msg: True,
+                health_check_fn=health_check_fn,
+            )
+
+        return _make_queue_consumer_factory
+
+    def test_new(self, baseplate, exchange, connection, name, routing_keys):
+        handler_fn = mock.Mock()
+        health_check_fn = mock.Mock()
+        factory = KombuQueueConsumerFactory.new(
+            baseplate=baseplate,
+            exchange=exchange,
+            connection=connection,
+            queue_name=name,
+            routing_keys=routing_keys,
+            handler_fn=handler_fn,
+            health_check_fn=health_check_fn,
+        )
+        assert factory.baseplate == baseplate
+        assert factory.connection == connection
+        assert factory.name == name
+        assert factory.handler_fn == handler_fn
+        assert factory.health_check_fn == health_check_fn
+        for routing_key, queue in zip(routing_keys, factory.queues):
+            assert queue.routing_key == routing_key
+            assert queue.name == name
+            assert queue.exchange == exchange
+
+    def test_build_pump_worker(self, make_queue_consumer_factory):
+        factory = make_queue_consumer_factory()
+        work_queue = Queue(maxsize=10)
+        pump = factory.build_pump_worker(work_queue)
+        assert isinstance(pump, KombuConsumerWorker)
+        assert pump.connection == factory.connection
+        assert pump.queues == factory.queues
+        assert pump.work_queue == work_queue
+
+    def test_build_message_handler(self, make_queue_consumer_factory):
+        factory = make_queue_consumer_factory()
+        handler = factory.build_message_handler()
+        assert isinstance(handler, KombuMessageHandler)
+        assert handler.baseplate == factory.baseplate
+        assert handler.name == factory.name
+        assert handler.handler_fn == factory.handler_fn
+
+    @pytest.mark.parametrize("health_check_fn", [None, lambda req: True])
+    def test_build_health_checker(self, health_check_fn, make_queue_consumer_factory):
+        factory = make_queue_consumer_factory(health_check_fn=health_check_fn)
+        listener = mock.Mock(spec=socket.socket)
+        health_checker = factory.build_health_checker(listener)
+        assert isinstance(health_checker, StreamServer)
+
+
+@pytest.fixture
+def queues(name, exchange, routing_keys):
+    return [kombu.Queue(name=name, exchange=exchange, routing_key=key) for key in routing_keys]
+
+
+class TestKombuConsumerWorker:
+    @pytest.fixture
+    def consumer_worker(self, connection, queues):
+        return KombuConsumerWorker(connection, queues, Queue(maxsize=10))
+
+    def test_initial_state(self, consumer_worker):
+        assert getattr(consumer_worker, "should_stop", None) is not True
+
+    @mock.patch("baseplate.frameworks.queue_consumer.kombu.ConsumerMixin.run")
+    def test_run(self, run, consumer_worker):
+        consumer_worker.run()
+        run.assert_called_once()
+
+    def test_stop(self, consumer_worker):
+        consumer_worker.stop()
+        assert consumer_worker.should_stop is True
