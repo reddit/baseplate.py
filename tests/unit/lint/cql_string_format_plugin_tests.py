dd5d25ddab1839740945cb7bce5cec326a029a42
Jenny Lam <jenny.lam@reddit.com>
2019-09-23 14:21:08 -0700

    update tests

--- tests/unit/lint/cql_string_format_plugin_tests.py
+++ tests/unit/lint/cql_string_format_plugin_tests.py
@@ -8,11 +8,10 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
     CHECKER_CLASS = cql_string_format_plugin.NoCQLStringFormatChecker
 
     def test_finds_variable_binop_string_format_query(self):
-        func_node, assign_node_a, call_node_b = astroid.extract_node(
+        assign_node_a, call_node_b = astroid.extract_node(
             """
-        def test(cassandra_session): #@
-            query = "select * from %s" % "users" #@
-            cassandra_session.execute(query) #@
+        query = "select * from %s" % "users" #@
+        cassandra_session.execute(query) #@
         """
         )
 
@@ -23,11 +22,10 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
         )
 
     def test_finds_variable_call_string_format_query(self):
-        func_node, assign_node_a, call_node_b = astroid.extract_node(
+        assign_node_a, call_node_b = astroid.extract_node(
             """
-        def test(cassandra_session): #@
-            query = "select * from %s".format("users") #@
-            cassandra_session.execute(query) #@
+        query = "select * from %s".format("users") #@
+        cassandra_session.execute(query) #@
         """
         )
 
@@ -38,10 +36,9 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
         )
 
     def test_finds_binop_string_format_query(self):
-        func_node, call_node_a = astroid.extract_node(
+        call_node_a = astroid.extract_node(
             """
-        def test(cassandra_session): #@
-            cassandra_session.execute("select * from %s" % "users") #@
+        cassandra_session.execute("select * from %s" % "users") #@
         """
         )
 
@@ -51,10 +48,9 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
         )
 
     def test_finds_call_string_format_query(self):
-        func_node, call_node_a = astroid.extract_node(
+        call_node_a = astroid.extract_node(
             """
-        def test(cassandra_session): #@
-            cassandra_session.execute("select * from %s".format("users")) #@
+        cassandra_session.execute("select * from %s".format("users")) #@
         """
         )
 
@@ -64,11 +60,10 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
         )
 
     def test_ignores_variable_non_string_format_query(self):
-        func_node, assign_node_a, call_node_b = astroid.extract_node(
+        assign_node_a, call_node_b = astroid.extract_node(
             """
-        def test(cassandra_session): #@
-            query = "select * from users" #@
-            cassandra_session.execute(query) #@
+        query = "select * from users" #@
+        cassandra_session.execute(query) #@
         """
         )
 
@@ -77,21 +72,19 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
             self.checker.visit_call(call_node_b)
 
     def test_ignores_non_string_format_query(self):
-        func_node, call_node_a = astroid.extract_node(
+        call_node_a = astroid.extract_node(
             """
-        def test(cassandra_session): #@
-            cassandra_session.execute("select * from users") #@
+        cassandra_session.execute("select * from users") #@
         """
         )
 
         with self.assertNoMessages():
             self.checker.visit_call(call_node_a)
 
-    def test_ignores_no_query(self):
-        func_node, call_node_a = astroid.extract_node(
+    def test_ignores_no_argument(self):
+        call_node_a = astroid.extract_node(
             """
-        def test(kb): #@
-            kb.execute() #@
+        kb.execute() #@
         """
         )
 
@@ -99,6 +92,20 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
             self.checker.visit_call(call_node_a)
 
     def test_variable_reset(self):
+        assign_node_a, assign_node_b, call_node_c = astroid.extract_node(
+            """
+        query = "select * from %s" % "users" #@
+        query = "select * from users" #@
+        cassandra_session.execute(query) #@
+        """
+        )
+
+        with self.assertNoMessages():
+            self.checker.visit_assign(assign_node_a)
+            self.checker.visit_assign(assign_node_b)
+            self.checker.visit_call(call_node_c)
+
+    def test_func_variable_reset(self):
         func_node, assign_node_a, assign_node_b, call_node_c = astroid.extract_node(
             """
         def test(cassandra_session): #@
@@ -114,5 +121,42 @@ class TestNoCQLStringFormatChecker(pylint.testutils.CheckerTestCase):
         with self.assertNoMessages():
             self.checker.visit_assign(assign_node_a)
             self.checker.leave_functiondef(func_node)
+            self.checker.visit_assign(assign_node_b)
+            self.checker.visit_call(call_node_c)
+
+    def test_class_variable_reset(self):
+        class_node, assign_node_a, assign_node_b, call_node_c = astroid.extract_node(
+            """
+        class Test(): #@
+            query = "select * from %s" % "users" #@
+            cassandra_session.execute(query)
+
+        query = "select * from users" #@
+        cassandra_session.execute(query) #@
+        """
+        )
+
+        with self.assertNoMessages():
+            self.checker.visit_assign(assign_node_a)
+            self.checker.leave_classdef(class_node)
+            self.checker.visit_assign(assign_node_b)
+            self.checker.visit_call(call_node_c)
+
+    def test_module_variable_reset(self):
+        mod_node, assign_node_a, assign_node_b, call_node_c = astroid.extract_node(
+            """
+        import test #@
+        query = "select * from %s" % "users" #@
+        cassandra_session.execute(query)
+
+        import other_test
+        query = "select * from users" #@
+        cassandra_session.execute(query) #@
+        """
+        )
+
+        with self.assertNoMessages():
+            self.checker.visit_assign(assign_node_a)
+            self.checker.leave_module(mod_node)
             self.checker.visit_assign(assign_node_b)
             self.checker.visit_call(call_node_c)
