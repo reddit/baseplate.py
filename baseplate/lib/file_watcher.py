3a67c9f7be5eb20d5dacc451d1871c6782848101
Laura Ungar <laura.ungar@reddit.com>
2019-10-09 10:54:05 -0700

    added documentation

--- baseplate/lib/file_watcher.py
+++ baseplate/lib/file_watcher.py
@@ -91,6 +91,8 @@ class FileWatcher(Generic[T]):
     :param newline: Controls how universal newlines mode works
         (it only applies to text mode). It can be `None`, `""`, `"\\n"`, `"\\r"`,
         and `"\\r\\n"`.  This is not supported if `binary` is set to `True`.
+    :param backoff: retry backoff time for the file watcher. Defaults to 
+        None, which is mapped to DEFAULT_FILEWATCHER_BACKOFF.
 
     """
 
