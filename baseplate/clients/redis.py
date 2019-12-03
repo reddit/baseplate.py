reddit/baseplate.py

commit 1aa93077e24041478b7c38d41c4c65a734bc8e76

Upgrade linters (#372)
A new pylint is required to handle the @overload declaration from the
typing module. This upgraded shared dependencies which forced an upgrade
of mypy. The changes to the redis module are because mypy got new
updates from the typeshed. The rest of the changes are because of
stricter checking.

baseplate/clients/redis.py
@@ -13,7 +13,7 @@ from baseplate.lib import message_queue
 
 def pool_from_config(
     app_config: config.RawConfig, prefix: str = "redis.", **kwargs: Any
-) -> redis.BlockingConnectionPool:
+) -> redis.ConnectionPool:
     """Make a ConnectionPool from a configuration dictionary.
 
     The keys useful to :py:func:`pool_from_config` should be prefixed, e.g.
@@ -113,15 +113,15 @@ class MonitoredRedisConnection(redis.StrictRedis):
 
         super().__init__(connection_pool=connection_pool)
 
-    # pylint: disable=arguments-differ
-    def execute_command(self, command: str, *args: Any, **kwargs: Any) -> Any:
+    def execute_command(self, *args: Any, **kwargs: Any) -> Any:
+        command = args[0]
         trace_name = f"{self.context_name}.{command}"
 
         with self.server_span.make_child(trace_name):
             return super().execute_command(command, *args, **kwargs)
 
     # pylint: disable=arguments-differ
-    def pipeline(
+    def pipeline(  # type: ignore
         self, name: str, transaction: bool = True, shard_hint: Optional[str] = None
     ) -> "MonitoredRedisPipeline":
         """Create a pipeline.
@@ -171,7 +171,8 @@ class MonitoredRedisPipeline(redis.client.StrictPipeline):
         self.server_span = server_span
         super().__init__(connection_pool, response_callbacks, **kwargs)
 
-    def execute(self, **kwargs: Any) -> Any:  # pylint: disable=arguments-differ
+    # pylint: disable=arguments-differ
+    def execute(self, **kwargs: Any) -> Any:  # type: ignore
         with self.server_span.make_child(self.trace_name):
             return super().execute(**kwargs)
 
@@ -211,7 +212,7 @@ class MessageQueue:
         if timeout == 0:
             message = self.client.lpop(self.queue)
         else:
-            message = self.client.blpop(self.queue, timeout=timeout)
+            message = self.client.blpop(self.queue, timeout=timeout or 0)
 
             if message:
                 message = message[1]
@@ -230,7 +231,7 @@ class MessageQueue:
                out of the queue as a string regardless of what type they are
                when passed into this method.
         """
-        return self.client.rpush(self.queue, message)
+        self.client.rpush(self.queue, message)
 
     def unlink(self) -> None:
         """Not implemented for Redis variant."""

baseplate/__init__.py
@@ -374,7 +374,10 @@ class Baseplate:
             from baseplate.observers.sentry import error_reporter_from_config
 
             if module_name is None:
-                module_name = inspect.getmodule(inspect.stack()[1].frame).__name__
+                module = inspect.getmodule(inspect.stack()[1].frame)
+                if not module:
+                    raise Exception("failed to detect module name, pass one explicitly")
+                module_name = module.__name__
 
             error_reporter = error_reporter_from_config(app_config, module_name)
             self.configure_error_reporting(error_reporter)

baseplate/lib/edge_context.py
@@ -91,7 +91,7 @@ class ValidatedAuthenticationToken(AuthenticationToken):
         return self.payload.get("sub")
 
     @cached_property
-    def user_roles(self) -> Set[str]:  # type: ignore
+    def user_roles(self) -> Set[str]:  # type: ignore # pylint: disable=W0236
         return set(self.payload.get("roles", []))
 
     @property

baseplate/lib/metrics.py
@@ -212,9 +212,9 @@ class Batch(BaseClient):
         exc_type: Optional[Type[BaseException]],
         value: Optional[BaseException],
         traceback: Optional[TracebackType],
-    ) -> bool:
+    ) -> None:
         self.flush()
-        return False  # don't swallow exception
+        return None  # don't swallow exception
 
     def flush(self) -> None:
         """Immediately send the batched metrics."""
@@ -311,9 +311,9 @@ class Timer:
         exc_type: Optional[Type[BaseException]],
         value: Optional[BaseException],
         traceback: Optional[TracebackType],
-    ) -> bool:
+    ) -> None:
         self.stop()
-        return False  # don't swallow exception
+        return None  # don't swallow exception
 
 
 class Counter:

baseplate/server/__init__.py
@@ -343,7 +343,7 @@ def load_and_run_shell() -> None:
         config = read_config(args.config_file, server_name=None, app_name=args.app_name)
     logging.basicConfig(level=logging.INFO)
 
-    env = dict()
+    env: Dict[str, Any] = {}
     env_banner = {
         "app": "This project's app instance",
         "context": "The context for this shell instance's span",

baseplate/sidecars/trace_publisher.py
@@ -79,7 +79,7 @@ class ZipkinPublisher:
             "User-Agent": "baseplate-trace-publisher/1.0",
             "Content-Type": "application/json",
         }
-        for time_remaining in RetryPolicy.new(attempts=self.retry_limit):
+        for _ in RetryPolicy.new(attempts=self.retry_limit):
             try:
                 with self.metrics.timer("post"):
                     response = self.session.post(

pylintrc
@@ -45,6 +45,7 @@ disable= attribute-defined-outside-init,
          duplicate-code,
          fixme,
          invalid-name,
+         import-outside-toplevel,
          line-too-long,
          missing-docstring,
          no-self-use,

commit 4fae4036107abc02ef14db9da9119536218bb06e

Fix redis client parameter munging and make test stricter
This regressed in 1aa93077e24041478b7c38d41c4c65a734bc8e76.

baseplate/clients/redis.py
@@ -118,7 +118,7 @@ class MonitoredRedisConnection(redis.StrictRedis):
         trace_name = f"{self.context_name}.{command}"
 
         with self.server_span.make_child(trace_name):
-            return super().execute_command(command, *args, **kwargs)
+            return super().execute_command(command, *args[1:], **kwargs)
 
     # pylint: disable=arguments-differ
     def pipeline(  # type: ignore

tests/integration/redis_tests.py
@@ -33,7 +33,9 @@ class RedisIntegrationTests(unittest.TestCase):
 
     def test_simple_command(self):
         with self.server_span:
-            self.context.redis.ping()
+            result = self.context.redis.ping()
+
+        self.assertTrue(result)
 
         server_span_observer = self.baseplate_observer.get_only_child()
         span_observer = server_span_observer.get_only_child()