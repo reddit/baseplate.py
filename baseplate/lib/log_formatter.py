83b3e29864e27c4285504b81eb532c3d5a4e151a
Jayme Howard <g.prime@gmail.com>
2020-06-15 17:56:09 -0500

    Apply suggestions from code review

--- baseplate/lib/log_formatter.py
+++ baseplate/lib/log_formatter.py
@@ -4,6 +4,8 @@ from pythonjsonlogger import jsonlogger
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def process_log_record(self, log_record: dict) -> dict:
         log_record["level"] = log_record.pop("levelname", None)
-        if "traceID" not in log_record and "threadName" in log_record:
-            log_record["traceID"] = log_record.get("threadName", None)
+        try:
+            log_record["traceID"] = int(log_record.get("threadName", None))
+        except ValueError:
+            pass
         return super().process_log_record(log_record)
