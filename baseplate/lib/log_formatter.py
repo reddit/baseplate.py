c5ea4b2288e16f7a78e80a32298d5bbd863ae3b6
Jayme Howard <g.prime@gmail.com>
2020-06-15 13:46:14 -0500

    Update baseplate/lib/log_formatter.py
    Co-authored-by: Neil Williams <spladug@users.noreply.github.com>

--- baseplate/lib/log_formatter.py
+++ baseplate/lib/log_formatter.py
@@ -4,4 +4,4 @@ from pythonjsonlogger import jsonlogger
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def process_log_record(self, log_record: dict) -> dict:
         log_record["level"] = log_record.pop("levelname", None)
-        return jsonlogger.JsonFormatter.process_log_record(self, log_record)
+        return super().process_log_record(log_record)
