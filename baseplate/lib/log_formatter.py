7714ff8b67cd4e953455b8f645d6b89480b16267
Jayme Howard <g.prime@gmail.com>
2020-06-15 18:41:14 -0500

    Update baseplate/lib/log_formatter.py
    Co-authored-by: Neil Williams <spladug@users.noreply.github.com>

--- baseplate/lib/log_formatter.py
+++ baseplate/lib/log_formatter.py
@@ -5,7 +5,7 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def process_log_record(self, log_record: dict) -> dict:
         log_record["level"] = log_record.pop("levelname", None)
         try:
-            log_record["traceID"] = int(log_record.get("threadName", None))
-        except (TypeError, ValueError):
+            log_record["traceID"] = int(log_record["threadName"])
+        except (KeyError, ValueError):
             pass
         return super().process_log_record(log_record)
