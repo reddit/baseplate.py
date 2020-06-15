22fda4df952880ef46473bb2ff0b6e2fe9a8eb8f
Jayme Howard <g.prime@gmail.com>
2020-06-15 13:50:38 -0500

    Update baseplate/lib/log_formatter.py

--- baseplate/lib/log_formatter.py
+++ baseplate/lib/log_formatter.py
@@ -4,4 +4,6 @@ from pythonjsonlogger import jsonlogger
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def process_log_record(self, log_record: dict) -> dict:
         log_record["level"] = log_record.pop("levelname", None)
+        if "traceID" not in log_record:
+            log_record["traceID"] = log_record.get("threadName", None)
         return super().process_log_record(log_record)
