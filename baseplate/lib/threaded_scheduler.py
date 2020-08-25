b2c06fd93e6423fa0b697406fb98097d8ca7ad6c
Frédérique Mittelstaedt <git@gfm.io>
2020-08-25 12:48:25 +0100

    lint

--- baseplate/lib/threaded_scheduler.py
+++ baseplate/lib/threaded_scheduler.py
@@ -1,34 +1,31 @@
-import threading
+from threading import Event
+from threading import Thread
 import time
+from typing import Optional
 
-from schedule import CancelJob
 from schedule import Scheduler
 
 
 class ThreadedScheduler(Scheduler):
-    """
-    A scheduler that runs continuously in a separate thread
-    """
+    """A scheduler that runs continuously in a separate thread."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self._stop_event = None
+        self._stop_event: Optional[Event] = None
 
-    def is_running(self):
+    def is_running(self) -> bool:
         return self._stop_event is not None
 
-    def run(self, interval=1):
-        """
-        Spawns a new thread and runs pending jobs at a regular interval
-        """
+    def run(self, interval: int = 1) -> None:
+        """Spawns a new thread and runs pending jobs at a regular interval."""
         if self.is_running():
             self.stop()
 
-        stop_event = threading.Event()
+        stop_event = Event()
 
-        class ScheduleThread(threading.Thread):
+        class ScheduleThread(Thread):
             @classmethod
-            def run(cls):
+            def run(cls) -> None:
                 while not stop_event.is_set():
                     self.run_pending()
                     time.sleep(interval)
@@ -38,7 +35,7 @@ class ThreadedScheduler(Scheduler):
 
         self._stop_event = stop_event
 
-    def stop(self):
+    def stop(self) -> None:
         if self._stop_event:
             self._stop_event.set()
             self._stop_event = None
