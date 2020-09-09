e00b48e0f8cff0f54e5753518de9bcda86d7abf4
Frédérique Mittelstaedt <git@gfm.io>
2020-09-09 11:10:02 +0100

    add BatchedQueue tests

---
+++ tests/unit/lib/batched_queue_tests.py
@@ -0,0 +1,70 @@
+from datetime import timedelta
+import pytest
+from queue import Queue
+from time import sleep
+from typing import Sequence
+
+from baseplate.lib.batched_queue import BatchedQueue
+
+@pytest.fixture
+def queue() -> Queue:
+    return Queue()
+
+
+class TestBatchedQueue:
+    def test_drain(self, queue: Queue[Sequence[int]]) -> None:
+        batched_queue: BatchedQueue[int] = BatchedQueue(queue, batch_size=5, flush_interval=timedelta(1))
+        batched_queue.put(0)
+        batched_queue.put(1)
+        batched_queue.put(2)
+        items = batched_queue.drain()
+        assert len(items) == 3 and items[0] == 0 and items[1] == 1 and items[2] == 2
+
+    def test_time_expiration(self, queue: Queue[Sequence[int]]) -> None:
+        batched_queue: BatchedQueue[int] = BatchedQueue(queue, batch_size=5, flush_interval=timedelta(0.1))
+        batched_queue.put(0)
+        batched_queue.put(1)
+        batched_queue.put(2)
+
+        sleep(0.2)
+
+        assert queue.qsize() == 1
+        batch = queue.get()
+        assert len(batch) == 3 and batch[0] == 0 and batch[1] == 1 and batch[2] == 2
+
+        items = batched_queue.drain()
+        assert len(items) == 0
+
+    def test_queue_limit(self, queue: Queue[Sequence[int]]) -> None:
+        batched_queue: BatchedQueue[int] = BatchedQueue(queue, batch_size=2, flush_interval=timedelta(1))
+        batched_queue.put(0)
+        batched_queue.put(1)
+        batched_queue.put(2)
+        batched_queue.put(3)
+        batched_queue.put(4)
+
+        assert queue.qsize() == 2
+        batch_1 = queue.get()
+        batch_2 = queue.get()
+        assert len(batch_1) == 2 and batch_1[0] == 0 and batch_1[1] == 1
+        assert len(batch_2) == 2 and batch_2[0] == 2 and batch_2[1] == 3
+
+        items = batched_queue.drain()
+        assert len(items) == 1 and items[0] == 4
+
+    def test_queue_limit_and_expiration(self, queue: Queue[Sequence[int]]) -> None:
+        batched_queue: BatchedQueue[int] = BatchedQueue(queue, batch_size=3, flush_interval=timedelta(1))
+        batched_queue.put(0)
+        batched_queue.put(1)
+        batched_queue.put(2)
+        batched_queue.put(3)
+        batched_queue.put(4)
+
+        assert queue.qsize() == 2
+        batch_1 = queue.get()
+        batch_2 = queue.get()
+        assert len(batch_1) == 3 and batch_1[0] == 0 and batch_1[1] == 1 and batch_1[2] == 2
+        assert len(batch_2) == 2 and batch_2[0] == 3 and batch_2[1] == 4
+
+        items = batched_queue.drain()
+        assert len(items) == 0
