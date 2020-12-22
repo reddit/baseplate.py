7ae0eac5be2bccea983133b4caf7ac49c2bac40a
Neil Williams <neil@reddit.com>
2020-12-22 12:36:45 -0800

    Remove experiments framework

--- baseplate/lib/experiments/targeting/base.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-
-class Targeting:
-    """Base targeting interface for experiment targeting."""
-
-    def evaluate(self, **kwargs: Any) -> bool:
-        """Evaluate whether the provided kwargs match the expected values for targeting."""
-        raise NotImplementedError
