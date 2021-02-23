d17d243dadce41fedc297edd282520a6d6a0c9a9
Brian Simpson <bsimpson63@gmail.com>
2021-02-23 10:52:32 -0800

    eeep

--- baseplate/lib/circuit_breaker/context.py
+++ baseplate/lib/circuit_breaker/context.py
@@ -12,7 +12,7 @@ from baseplate.lib.circuit_breaker.observer import BreakerObserver
 
 """
 Notes:
-right now each remove service will generally have its own breaker
+right now each remote service will generally have its own breaker
 failures in any of its endpoints will be treated equally
 should we have some more fine grained control? separate counters per
 endpoint (if desired) plus tracking of overall error rate?
