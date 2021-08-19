a2944dc968b5f78db1093654cd7ead3880f549c4
Neil Williams <neil@reddit.com>
2021-08-19 09:55:51 -0700

    Officially support Python 3.9
    Update the dev env, test suites, and metadata.

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
     ],
