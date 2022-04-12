093fbf4cc1699d244a3d022eeee507b08ce7b18d
Siddharth Manoj <smanoj@usc.edu>
2022-04-12 16:16:53 -0700

    Support loading encrypted contents from S3 bucket for live_data_watch (#658)
    * Add live data watcher logic to fetch URL
    
    * formatting
    
    * GCP->GCS
    
    * change exception to debug
    
    * Add boto3 api
    
    * remove request url download method
    
    * add static method annotation
    
    * remove unused import
    
    * fix return type
    
    * return bytes
    
    * no self
    
    * format
    
    * return type
    
    * return type
    
    * rename
    
    * add unit tests
    
    * logger.error
    
    * address tyler comments
    
    * fix some typing complaints
    
    * typing
    
    * move to requirements.txt
    
    * address roman comments
    
    * dont check exception in test
    
    * fix none type handling
    
    * add return type
    
    * better exception logs
    
    * typing issues
    
    * more typing issues
    
    * don't delete dest
    
    * add tests again
    
    * remove lc
    
    Co-authored-by: Sid Manoj <sid.manoj@reddit.com>

--- baseplate/sidecars/live_data_watcher.py
+++ baseplate/sidecars/live_data_watcher.py
@@ -1,6 +1,7 @@
 """Watch nodes in ZooKeeper and sync their contents to disk on change."""
 import argparse
 import configparser
+import json
 import logging
 import os
 import sys
@@ -9,7 +10,11 @@ import time
 from pathlib import Path
 from typing import Any
 from typing import NoReturn
+from typing import Optional
 
+import boto3  # type: ignore
+
+from botocore.client import ClientError  # type: ignore
 from kazoo.client import KazooClient
 from kazoo.protocol.states import ZnodeStat
 
@@ -32,14 +37,78 @@ class NodeWatcher:
         self.group = group
         self.mode = mode
 
-    def on_change(self, data: bytes, _znode_stat: ZnodeStat) -> None:
-        if data is None:
-            # the data node does not exist
+    @staticmethod
+    def get_encrypted_data_from_s3(
+        bucket_name: str, file_key: str, region_name: str, sse_key: str
+    ) -> Optional[bytes]:
+        s3_client = boto3.client(
+            "s3",
+            region_name=region_name,
+        )
+        data = None
+        # The S3 data must be Server Side Encrypted and we should have the decryption key.
+        kwargs = {
+            "Bucket": bucket_name,
+            "Key": file_key,
+            "SSECustomerKey": sse_key,
+            "SSECustomerAlgorithm": "AES256",
+        }
+        try:
+            s3_object = s3_client.get_object(**kwargs)
+            # Returns bytes.
+            data = s3_object["Body"].read()
+        except ClientError as error:
+            logger.exception(
+                "Failed to update live config: failed to load data from S3. Received error code %s: %s",
+                error.response["Error"]["Code"],
+                error.response["Error"]["Message"],
+            )
+            return None
+        except ValueError as error:
+            logger.exception(
+                "Failed to update live config: params for loading from S3 are incorrect. Received error: %s",
+                error,
+            )
+            return None
+        return data
+
+    @staticmethod
+    def get_data_to_write(json_data: dict, data: Optional[bytes]) -> Optional[bytes]:
+        # Check if we have a JSON in a special format containing:
+        # data = {
+        #    "live_data_watcher_load_type": str
+        # }
+        # If the load type is 'S3', this format is an indication that we support
+        # downloading the contents of encrypted files uploaded to S3.
+        if json_data.get("live_data_watcher_load_type") == "S3":
             try:
-                logger.info("Removing %r; watched node deleted.", self.dest)
-                os.unlink(self.dest)
-            except OSError as exc:
-                logger.debug("%s: couldn't unlink: %s", self.dest, exc)
+                bucket_name = json_data["bucket_name"]
+                file_key = json_data["file_key"]
+                sse_key = json_data["sse_key"]
+                region_name = json_data["region_name"]
+            except KeyError:
+                # We require all of these keys to properly read from S3.
+                logger.exception(
+                    "Failed to update live config: unble to fetch content from s3: source config has invalid or missing keys."
+                )
+                return None
+            # If we have all the correct keys, attempt to read the config from S3.
+            data = NodeWatcher.get_encrypted_data_from_s3(
+                bucket_name=bucket_name, file_key=file_key, region_name=region_name, sse_key=sse_key
+            )
+        return data
+
+    def handle_empty_data(self) -> None:
+        # the data node does not exist
+        try:
+            logger.info("Removing %r; watched node deleted.", self.dest)
+            os.unlink(self.dest)
+        except OSError as exc:
+            logger.debug("%s: couldn't unlink: %s", self.dest, exc)
+
+    def on_change(self, data: Optional[bytes], _znode_stat: ZnodeStat) -> None:
+        if data is None:
+            self.handle_empty_data()
             return
 
         # swap out the file atomically so clients watching the file never catch
@@ -50,8 +119,22 @@ class NodeWatcher:
             if self.owner and self.group:
                 os.fchown(tmpfile.fileno(), self.owner, self.group)
             os.fchmod(tmpfile.fileno(), self.mode)
-
-            tmpfile.write(data)
+            try:
+                json_data = json.loads(data.decode("UTF-8"))
+            except json.decoder.JSONDecodeError:
+                # If JSON fails to decode, still write the bytes data since
+                # we don't necessarily know if the the contents of the znode
+                # had to be valid JSON anyways.
+                tmpfile.write(data)
+            else:
+                # If no exceptions, we have valid JSON, and can parse accordingly.
+                data = NodeWatcher.get_data_to_write(json_data, data)
+                if data is None:
+                    logger.warning(
+                        "No data written to destination file. Something is likely misconfigured."
+                    )
+                    return
+                tmpfile.write(data)
         os.rename(self.dest + ".tmp", self.dest)
 
 
--- tests/unit/sidecars/live_data_watcher_tests.py
+++ tests/unit/sidecars/live_data_watcher_tests.py
@@ -1,4 +1,5 @@
 import grp
+import json
 import os
 import pwd
 import tempfile
@@ -6,14 +7,65 @@ import unittest
 
 from pathlib import Path
 
+import boto3
+
+from moto import mock_s3
+
+from baseplate.sidecars.live_data_watcher import logger
 from baseplate.sidecars.live_data_watcher import NodeWatcher
 
 
 class NodeWatcherTests(unittest.TestCase):
+    mock_s3 = mock_s3()
+
+    def setUp(self):
+        self.mock_s3.start()
+        bucket_name = "test_bucket"
+        s3_data = {"foo_encrypted": "bar_encrypted"}
+        s3_client = boto3.client(
+            "s3",
+            region_name="us-east-1",
+        )
+        s3_client.create_bucket(Bucket=bucket_name)
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Key="test_file_key",
+            Body=json.dumps(s3_data).encode(),
+            SSECustomerKey="test_decryption_key",
+            SSECustomerAlgorithm="AES256",
+        )
+
+    def tearDown(self):
+        self.mock_s3.stop()
+
     def run(self, result: unittest.TestResult = None) -> unittest.TestResult:
         with tempfile.TemporaryDirectory(prefix=self.id()) as loc:
             self.output_dir = Path(loc)
             return super().run(result)
+
+    def test_s3_load_type_on_change(self):
+        dest = self.output_dir.joinpath("data.txt")
+        inst = NodeWatcher(str(dest), os.getuid(), os.getgid(), 777)
+
+        new_content = b'{"live_data_watcher_load_type":"S3","bucket_name":"test_bucket","file_key":"test_file_key","sse_key":"test_decryption_key","region_name":"us-east-1"}'
+        expected_content = b'{"foo_encrypted": "bar_encrypted"}'
+        inst.on_change(new_content, None)
+        self.assertEqual(expected_content, dest.read_bytes())
+        self.assertEqual(dest.owner(), pwd.getpwuid(os.getuid()).pw_name)
+        self.assertEqual(dest.group(), grp.getgrgid(os.getgid()).gr_name)
+
+    def test_s3_load_type_missing_sse_key_on_change(self):
+        with self.assertLogs(logger.name, level="DEBUG") as lc:
+            dest = self.output_dir.joinpath("data.txt")
+            inst = NodeWatcher(str(dest), os.getuid(), os.getgid(), 777)
+
+            new_content = b'{"live_data_watcher_load_type":"S3","bucket_name":"test_bucket","file_key":"test_file_key","region_name":"us-east-1"}'
+            inst.on_change(new_content, None)
+        self.assertIn(
+            "WARNING:%s:No data written to destination file. Something is likely misconfigured."
+            % (logger.name),
+            lc.output,
+        )
 
     def test_on_change(self):
         dest = self.output_dir.joinpath("data.txt")
@@ -24,6 +76,14 @@ class NodeWatcherTests(unittest.TestCase):
         self.assertEqual(new_content, dest.read_bytes())
         self.assertEqual(dest.owner(), pwd.getpwuid(os.getuid()).pw_name)
         self.assertEqual(dest.group(), grp.getgrgid(os.getgid()).gr_name)
+
+    def test_on_change_no_data(self):
+        dest = self.output_dir.joinpath("data.txt")
+        inst = NodeWatcher(str(dest), os.getuid(), os.getgid(), 777)
+
+        new_content = None
+        inst.on_change(new_content, None)
+        self.assertEqual(False, os.path.exists(dest))
 
     def test_on_change_new_dir(self):
         dest = self.output_dir.joinpath("data/output.json")
