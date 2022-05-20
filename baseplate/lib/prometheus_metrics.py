baa715cab6ddb069ea45118667fe2db60ceb3663
Nathan Heaps <nathan.heaps@reddit.com>
2022-05-20 00:22:44 -0400

    last bit of linting

--- baseplate/lib/prometheus_metrics.py
+++ baseplate/lib/prometheus_metrics.py
@@ -504,5 +504,5 @@ def get_metrics_for_prefix(prefix: str) -> PrometheusGenericSpanMetrics:
                 ),
             },
         )
-        logger.info("Created new metrics class for prefix {}" % prefix)
+        logger.info("Created new metrics class for prefix %s", prefix)
     return generic_metrics[prefix]
