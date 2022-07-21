a1bb63071e5196a636d745cd87bb43d1cb30ee2e
Natalie Swan <natalie.swan@reddit.com>
2022-07-21 17:05:35 -0400

    Add tests for MonitoredRedisConnection.execute_command

--- tests/unit/clients/redis_tests.py
+++ tests/unit/clients/redis_tests.py
@@ -1,6 +1,10 @@
 import os
-import pytest
+import unittest
+
 from unittest import mock
+
+import pytest
+
 from prometheus_client import REGISTRY
 
 try:
@@ -9,7 +13,7 @@ except ImportError:
     raise unittest.SkipTest("redis-py is not installed")
 else:
     del redis
-from redis import StrictRedis
+from redis.exceptions import ConnectionError
 
 from baseplate.lib.config import ConfigurationError
 from baseplate.clients.redis import pool_from_config
@@ -17,21 +21,7 @@ from baseplate.clients.redis import ACTIVE_REQUESTS
 from baseplate.clients.redis import REQUESTS_TOTAL
 from baseplate.clients.redis import LATENCY_SECONDS
 from baseplate.clients.redis import MonitoredRedisConnection
-# from baseplate import RequestContext
-# from baseplate import ServerSpan
-from baseplate import Span
-from baseplate.testing.lib.secrets import FakeSecretsStore
 
-
-@pytest.fixture
-def redis_client():
-    def baseplate_pipeline_adapter(self, name, transaction=True, shard_hint=None):
-        return StrictRedis.pipeline(self, transaction=transaction, shard_hint=shard_hint)
-
-    client = StrictRedis(host="redis", port=6379, db=1)
-    client.flushdb()
-    client.pipeline = baseplate_pipeline_adapter.__get__(client)
-    return client
 
 class DummyConnection:
     description_format = "DummyConnection<>"
@@ -52,8 +42,8 @@ class DummyConnection:
     def read_response(self):
         pass
 
-class TestMonitoredRedisConnection:
 
+class TestMonitoredRedisConnection:
     def setup(self):
         ACTIVE_REQUESTS.clear()
         REQUESTS_TOTAL.clear()
@@ -62,30 +52,26 @@ class TestMonitoredRedisConnection:
     @pytest.fixture
     def app_config(self):
         yield {
-                "redis.url": "redis://localhost:1234/0",
-                "redis.socket_timeout": "30 seconds",
-                "redis.socket_connect_timeout": "300 milliseconds",
+            "redis.url": "redis://localhost:1234/0",
+            # "redis.socket_timeout": "30 seconds",
+            # "redis.socket_connect_timeout": "300 milliseconds",
         }
 
     @pytest.fixture
     def expected_labels(self):
         yield {
-            "redis_address": "localhost", # check host vs address
+            "redis_address": "localhost",
             "redis_command": "some_command",
             "redis_database": "0",
         }
 
     @pytest.fixture
-    def secrets(self):
-        return {
-                        "connection_class": DummyConnection
-                    }
-
-        
+    def connection(self):
+        return {"connection_class": DummyConnection}
 
     @pytest.fixture
-    def connection_pool(self, app_config, secrets):
-        yield pool_from_config(app_config=app_config, prefix="redis.", **secrets)
+    def connection_pool(self, app_config, connection):
+        yield pool_from_config(app_config=app_config, prefix="redis.", **connection)
 
     @pytest.fixture
     def context(self):
@@ -99,28 +85,34 @@ class TestMonitoredRedisConnection:
     def monitored_redis_connection(self, span, connection_pool):
         yield MonitoredRedisConnection("redis_context_name", span, connection_pool)
 
-    def test_execute_command_exc(self, monitored_redis_connection, expected_labels):
-        # with pytest.raises(Exception, match=r"^Error"):
-        # with mock.patch.object(
-        #     monitored_redis_connection.connection, "send_command", wraps=redis_client.connection.send_command
-        # ) as m:
-        #     r.get("foo")
-        #     m.assert_called_with("some_command", check_health=False)
-        monitored_redis_connection.execute_command("some_command")
-        # assert "" == [m for m in REGISTRY.collect()]
-        # assert (
-        #     REGISTRY.get_sample_value(f"{ACTIVE_REQUESTS._name}", expected_labels) == 0
-        # )
-        expected_labels["redis_success"] = "true"
-        # assert (
-        #     REGISTRY.get_sample_value(
-        #         f"{LATENCY_SECONDS._name}_bucket", {**expected_labels, **{"le": "+Inf"}}
-        #     )
-        #     == 1
-        # )
+    def test_execute_command_exc_redis_err(
+        self, monitored_redis_connection, expected_labels, app_config
+    ):
+        monitored_redis_connection.connection_pool = pool_from_config(app_config=app_config)
+        with pytest.raises(ConnectionError):  # ConnectionError inherits from RedisError
+            monitored_redis_connection.execute_command("some_command")
+        assert REGISTRY.get_sample_value(f"{ACTIVE_REQUESTS._name}", expected_labels) == 0
+        expected_labels["redis_success"] = "false"
         assert (
-            REGISTRY.get_sample_value(f"{REQUESTS_TOTAL._name}_total", expected_labels) == 1
+            REGISTRY.get_sample_value(
+                f"{LATENCY_SECONDS._name}_bucket", {**expected_labels, **{"le": "+Inf"}}
+            )
+            == 1
         )
+        assert REGISTRY.get_sample_value(f"{REQUESTS_TOTAL._name}_total", expected_labels) == 1
+
+    def test_execute_command(self, monitored_redis_connection, expected_labels):
+        monitored_redis_connection.execute_command("some_command")
+        assert REGISTRY.get_sample_value(f"{ACTIVE_REQUESTS._name}", expected_labels) == 0
+        expected_labels["redis_success"] = "true"
+        assert (
+            REGISTRY.get_sample_value(
+                f"{LATENCY_SECONDS._name}_bucket", {**expected_labels, **{"le": "+Inf"}}
+            )
+            == 1
+        )
+        assert REGISTRY.get_sample_value(f"{REQUESTS_TOTAL._name}_total", expected_labels) == 1
+
 
 class TestPoolFromConfig:
     def test_empty_config(self):
