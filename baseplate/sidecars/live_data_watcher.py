3e0bf353c0344dce4551bbc96014bb898b024c4b
siddysidsid <sid.manoj@reddit.com>
2022-08-21 14:59:03 -0400

    line length

--- baseplate/sidecars/live_data_watcher.py
+++ baseplate/sidecars/live_data_watcher.py
@@ -137,10 +137,12 @@ def _load_from_s3(data: bytes) -> bytes:
         )
         raise LoaderException from e
 
-    # Client needs to be anonymous/unsigned or boto3 will try to read the local credentials on the service pods.
-    # And - due to an AWS quirk - any request that comes in signed with credentails will profile for permissions
-    # for the resource being requested EVEN if the resource is public.
-    # In other words, this means that a given service could not access public resources belonging to another cluster/AWS account unless the request credentials were unsigned.
+    # Client needs to be anonymous/unsigned or boto3 will try to read the local credentials 
+    # on the service pods. And - due to an AWS quirk - any request that comes in signed with 
+    # credentails will profile for permissions for the resource being requested EVEN if the 
+    # resource is public. In other words, this means that a given service could not access 
+    # public resources belonging to another cluster/AWS account unless the request credentials 
+    # were unsigned.
     s3_client = boto3.client(
         "s3",
         config=Config(signature_version=UNSIGNED),
