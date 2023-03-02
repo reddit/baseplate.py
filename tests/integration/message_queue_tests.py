53a4df02006391a1d8ad6a96c6678d63a0234441
syd ryan <syd.ryan@reddit.com>
2023-03-02 14:18:50 -0800

    x

--- tests/integration/message_queue_tests.py
+++ tests/integration/message_queue_tests.py
@@ -187,7 +187,9 @@ class TestRemoteMessageQueueCreation(GeventPatchedTestCase):
     def test_get_timeout(self):
         queues = {}
         with publisher_queue_utils.start_queue_server(queues, host="127.0.0.1", port=9090):
-            _ = RemoteMessageQueue(self.qname, max_messages=1, pool_size=1) # create the empty queue
+            _ = RemoteMessageQueue(
+                self.qname, max_messages=1, pool_size=1
+            )  # create the empty queue
 
             start = time.time()
             with self.assertRaises(TimedOutError):
