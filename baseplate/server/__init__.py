392e92dbc6a535c743fd6ece2dadcf699c393e49
Trevor Riles <trevor@trevorriles.com>
2023-09-22 15:01:22 -0500

    Add log lines

--- baseplate/server/__init__.py
+++ baseplate/server/__init__.py
@@ -205,7 +205,9 @@ def configure_logging(config: Configuration, debug: bool) -> None:
 
 
 def configure_tracing(config: Configuration) -> None:
+    logger.info("Entering configure tracing function"
     if config.tracing:
+        logger.info("Tracing config has been provided")
         if "service_name" in config.tracing and "endpoint" in config.tracing:
             resource = Resource.create({SERVICE_NAME: config.tracing["service_name"]})
             # Don't sample unless parent sampled by default
