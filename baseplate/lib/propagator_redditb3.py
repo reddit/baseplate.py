8924c8b7b3fb23779afa8f6f7c0e4e63b322dc46
Trevor Riles <trevor@trevorriles.com>
2023-11-30 18:06:13 -0600

    Fix optional type to include module

--- baseplate/lib/propagator_redditb3.py
+++ baseplate/lib/propagator_redditb3.py
@@ -124,7 +124,7 @@ class RedditB3Format(TextMapPropagator):
 
 def _extract_first_element(
     items: typing.Iterable[CarrierT],
-    default: Optional[Any] = None,
+    default: typing.Optional[typing.Any] = None,
 ) -> typing.Optional[CarrierT]:
     if items is None:
         return default
