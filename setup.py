c5dcaaef6ffa3c6fbe86a95fd797c400e333a643
Alexis Lesieur <Xaelias@users.noreply.github.com>
2023-12-18 14:19:37 -0600

    no 3.10

--- setup.py
+++ setup.py
@@ -77,7 +77,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
     ],
