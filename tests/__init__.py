90d03b91d61014207e7a0dcaa45f69e13987e1a1
Alexis Lesieur <Xaelias@users.noreply.github.com>
2023-12-18 13:50:23 -0600

    Remove support for python 3.7, addn python 3.10 and 3.11

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,4 @@
-try:
-    # nullcontext is only available in Python 3.7+
-    from contextlib import nullcontext as does_not_raise
-except ImportError:
-    from contextlib import contextmanager
-
-    @contextmanager
-    def does_not_raise():
-        yield
-
+from contextlib import nullcontext as does_not_raise
 
 __all__ = [
     "does_not_raise",
