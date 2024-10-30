3a7f4b74cfcdd651d35475d03cc41d291071e6b8
Trevor Riles <trevor@reddit.com>
2024-10-30 12:52:18 -0500

    Remove log formatting

--- baseplate/lib/log_formatter.py
+++ baseplate/lib/log_formatter.py
@@ -5,10 +5,4 @@ from pythonjsonlogger import jsonlogger
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def process_log_record(self, log_record: dict) -> dict:
         log_record["level"] = log_record.pop("levelname", None)
-        try:
-            span = trace.get_current_span()
-            if span.is_recording():
-                log_record["traceID"] = trace.format_trace_id(span.get_span_context().trace_id)
-        except (KeyError, ValueError, TypeError):
-            pass
         return super().process_log_record(log_record)
